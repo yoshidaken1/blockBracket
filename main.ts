40090bde59df6147eea4326dcf7fd10c5b24b4c9
Kenichi Yoshida <dietposter@gmail.com>
2018-09-09 10:58:33 +0900

    update 1st pxt-comment

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% weight=70 icon="\uf075" color=#555555 block="コメント"
+namespace comment {
+    //% blockId=show_strings block="コメント %v"
+    export function noaction(text: string): void {
+    }
+}
